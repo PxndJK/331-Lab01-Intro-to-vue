eced247dca9aadad5fa096473af28357738a62a3
Pattanachai_n <pondlala33@gmail.com>
2023-07-01 23:57:54 +0700

    4.9 commit

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ createApp({
     setup(){
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
+        const inStock = ref(false)
+        const inventory = ref(100)
+        const onSale = ref(false)
         const description = ref('green sock so beautiful')
         const link =ref('http://www.camt.cmu.ac.th')
         return{
-            product,image,description , link
+            product,image,description,link,inStock,inventory,onSale
         }
     }
 
