48410591cd4d2bb269d3b6d936da834d26961b06
Pattanachai_n <pondlala33@gmail.com>
2023-07-02 00:39:36 +0700

    6.7 commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ createApp({
     setup(){
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
-        const inStock = ref(false)
+        const inStock = ref(true)
         const inventory = ref(100)
         const details = ref([
             '50% cotton',
@@ -12,9 +12,19 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234, color: 'green'},
-            { id: 2235, color: 'blue'}
+            { id: 2234, color: 'green' , image: './assets/images/socks_green.jpg'},
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg'}
         ])
+        const cart = ref(0)
+        function addToCart(){
+            cart.value +=1
+        }
+        function changeStock(){
+            inStock.value = !inStock.value
+        }
+        function updateImage(variantImage){
+            image.value = variantImage
+        }
         const size = ref([
             'S','M','L'
         ])
@@ -22,7 +32,8 @@ createApp({
         const description = ref('green sock so beautiful')
         const link =ref('http://www.camt.cmu.ac.th')
         return{
-            product,image,description,link,inStock,inventory,onSale,details,variants,size
+            product,image,description,link,inStock,inventory,onSale,details,variants,
+            size,cart,addToCart,updateImage,changeStock
         }
     }
 
