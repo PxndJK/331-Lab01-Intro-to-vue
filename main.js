412aee81a74862019db20b70858584c6c597bdd7
Pattanachai_n <pondlala33@gmail.com>
2023-07-01 23:41:23 +0700

    3.6 commit

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ const {createApp , ref} = Vue
 createApp({
     setup(){
         const product = ref('Boots')
+        const image = ref('./assets/images/socks_green.jpg')
         const product1 = ref('shoes')
+        const link =ref('http://www.camt.cmu.ac.th')
         return{
-            product , product1
+            product,image,product1 , link
         }
     }
 
